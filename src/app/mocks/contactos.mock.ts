2e2b6d41bd155e6e771c7b4e5167d91bf15f56e2
Cristhian González <cristhian.gonzalez@udea.edu.co>
2023-01-01 18:19:25 -0500

    Creating a 'contacto' list as a mock

---
+++ src/app/mocks/contactos.mock.ts
@@ -0,0 +1,26 @@
+import { IContacto } from '../models/contacto.interface';
+
+// Exportamos una lista simulada de Contactos
+export const CONTACTOS: IContacto[] = [
+  {
+    id: 0,
+    nombre: 'Martín',
+    apellidos: 'San José',
+    email: 'martin@imaginagroup.com',
+    edad: 30,
+  },
+  {
+    id: 1,
+    nombre: 'Juan',
+    apellidos: 'Pérez',
+    email: 'juan@jijijija.com',
+    edad: 25,
+  },
+  {
+    id: 2,
+    nombre: 'Cris',
+    apellidos: 'González',
+    email: 'cris@cris.com',
+    edad: 23,
+  },
+];
