840a46723b9bb4ebd535f936c1b68133ff1955bd
Cristhian González <cristhian.gonzalez@udea.edu.co>
2023-01-01 18:23:01 -0500

    Adding the 'listaContactos' component to the main app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SaludoComponent } from './components/saludo/saludo.component';
 import { ListsModule } from './modules/lists/lists.module';
+import { ListaContactosComponent } from './components/lista-contactos/lista-contactos.component';
 
 @NgModule({
-  declarations: [AppComponent, SaludoComponent],
+  declarations: [AppComponent, SaludoComponent, ListaContactosComponent],
   imports: [BrowserModule, FormsModule, ListsModule],
   providers: [],
   bootstrap: [AppComponent],
