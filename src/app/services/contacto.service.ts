a37c8b5500e1f31d49919c2dca8ec0fc9d3bf6a7
Cristhian González <cristhian.gonzalez@udea.edu.co>
2023-01-01 20:06:48 -0500

    Implements Observables in contactoService

--- src/app/services/contacto.service.ts
+++ src/app/services/contacto.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
 import { CONTACTOS } from '../mocks/contactos.mock';
 import { IContacto } from '../models/contacto.interface';
 
+//Importamos Oberservables de rxjs
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -14,14 +17,20 @@ export class ContactoService {
     return Promise.resolve(CONTACTOS); // Nos devuelve un listado de contactos
   }
 
-  obtenerContactoPorID(id: number): Promise<IContacto> | undefined {
+  obtenerContactoPorID(id: number): Observable<IContacto> | undefined {
     // Buscamos el contacto por ID dentro de la lista de CONTACTOS mockeados
     const contacto = CONTACTOS.find(
       (contacto: IContacto) => contacto.id === id
     );
 
+    // Creamos un observable que nos devuelva el contacto
+    let observable: Observable<IContacto> = new Observable((observer) => {
+      observer.next(contacto); // Emitir un valor a los componentes que estén suscritos
+      observer.complete(); // Indicar que la emisión de valores ha terminado
+    });
+
     if (contacto) {
-      return Promise.resolve(contacto);
+      return observable;
     } else {
       return;
     }
