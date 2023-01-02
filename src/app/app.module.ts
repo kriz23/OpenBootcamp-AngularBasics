01424832c496a28fd279dabbdb1961a474347809
Cristhian González <cristhian.gonzalez@udea.edu.co>
2023-01-01 20:12:46 -0500

    Adding the HttpClientModule and loginForm component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { SaludoComponent } from './components/saludo/saludo.component';
 import { ListsModule } from './modules/lists/lists.module';
 import { ListaContactosComponent } from './components/lista-contactos/lista-contactos.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginFormComponent } from './components/forms/login-form/login-form.component';
 
 @NgModule({
-  declarations: [AppComponent, SaludoComponent, ListaContactosComponent],
-  imports: [BrowserModule, FormsModule, ListsModule],
+  declarations: [AppComponent, SaludoComponent, ListaContactosComponent, LoginFormComponent],
+  imports: [BrowserModule, FormsModule, ListsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
